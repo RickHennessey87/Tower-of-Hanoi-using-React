ad311ed41c9f21f05da11d70d30683cb927e6b9e
Patrick Hennessey <pjhennessey89@gmail.com>
2025-01-03 10:29:37 -0600

    Added Peg Component

--- Tower-of-Hanoi/src/components/Peg/Peg.jsx
+++ Tower-of-Hanoi/src/components/Peg/Peg.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import { useDrop } from 'react-dnd';
+import { Disk } from '../Disk/Disk';
 
 export const Peg = ({ pegIndex, disks, moveDisk }) => {
-    
-}
+    const topDiskOnPeg = disks[disks.length - 1] || Infinity;
+
+    const [{ isOver, canDrop }, drop] = useDrop(() => ({
+        accept: 'DISK',
+        canDrop: (item) => {
+            return item.size < topDiskOnPeg;
+        },
+        drop: (item) => {
+            moveDisk(item.fromPeg, pegIndex, item.size);
+        },
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+    }));
+
+    const style = {
+        width: '150px',
+        minHeight: '200px',
+        border: '2px solid black',
+        margin: '10px',
+        backgroundColor: isOver && canDrop ? 'lightgreen' : 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end', 
+        alignItems: 'center',
+    };
+
+    return (
+        <div ref={drop} style={style}>
+            {disks.map((disk, idx) => {
+                const isTopDisk = idx === disks.length - 1;
+                return (
+                    <Disk
+                        key={disk}
+                        size={disk}
+                        pegIndex={pegIndex}
+                        topDisk={isTopDisk}
+                    >
+                        Disk {disk}
+                    </Disk>
+                );
+            })}
+        </div>
+    );
+};
